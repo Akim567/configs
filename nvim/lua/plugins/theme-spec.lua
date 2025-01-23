ae424ab7409b34ea692d67190362eb9429d5c2d4
Akim <akim.bartsits@mail.ru>
2025-01-23 17:12:42 +0300

    Отредактирован конфиг

--- nvim/lua/plugins/theme-spec.lua
+++ nvim/lua/plugins/theme-spec.lua
@@ -3,10 +3,29 @@ return {
         'ellisonleao/gruvbox.nvim',
         name = 'gruvbox',
         lazy = false,
-        priority = 2000,
-        config = function()
-            vim.cmd([[colorscheme gruvbox]])
-        end,
-        -- opts = {},
+        priority = 1000,
+		config = function()
+			vim.api.nvim_create_autocmd("VimEnter", {
+			   callback = function()
+			     vim.cmd([[colorscheme gruvbox]])
+			   end,
+    })
+	end,
+        opts = {},
+    },
+    {
+        'folke/tokyonight.nvim',
+        name = 'tokyonight',
+        lazy = true,
+        priority = 1000,
+        opts = {},
+    },
+    {
+        'catppuccin/nvim',
+        name = 'catppuccin',
+        lazy = true,
+        priority = 1000,
+        opts = {},
     },
 }
+
