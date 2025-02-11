1ac75254120f6e8dfbfd19c6170651adac64071d
Akim <akim.bartsits@mail.ru>
2025-02-11 09:48:31 +0300

    Исправлено кнопка win и ctrl

--- keyboard/keymap.c
+++ keyboard/keymap.c
@@ -40,8 +40,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_QWERTY] = LAYOUT(
   KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                                             KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_GRV,
-  MT(MOD_LCTL, KC_RBRC),   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                              KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_LBRC,
-  MT(MOD_LGUI, KC_GRV),   KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,  MT(MOD_LGUI, KC_QUOT),
+  MT(MOD_LGUI, KC_RBRC),   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                              KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_LBRC,
+  MT(MOD_LCTL, KC_GRV),   KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,  MT(MOD_LGUI, KC_QUOT),
   MT(MOD_LALT, KC_MINS),  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_MUTE,          XXXXXXX,KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,   MT(MOD_LALT, KC_BSLS),
        //5     //4           //3                  //2                  //1
      KC_LGUI,XXXXXXX,LT(_RAISE, KC_TAB), MT(MOD_LSFT, KC_SPC), LT(_LOWER, KC_ENT),      LT(_LOWER, KC_ESC),  MT(MOD_RSFT, KC_BSPC), LT(_RAISE, KC_DEL), KC_RALT, KC_RGUI
